04b52fd241de8dc480c5a8a83d4cd18f1dc8389d
hemanth <hemanth@vyaparapp.in>
2022-12-25 15:19:28 +0530

    Initial Commit

---
+++ GuessCountries/GuessCountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessCountriesApp.swift
+//  GuessCountries
+//
+//  Created by HEMANTH on 25/12/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessCountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
